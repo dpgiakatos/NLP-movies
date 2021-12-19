ee8d13cf419fd11313bc94d4fc74422c580d0100
Dimitrios Panteleimon Giakatos <dgiakatos@csd.auth.gr>
2021-12-19 16:01:06 +0200

    task 1-3 done

--- main.py
+++ main.py
@@ -1,37 +1,109 @@
-import requests
-import csv
+import pandas as pd
 import json
-import datetime
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from sklearn.model_selection import train_test_split
 
 
-def collect_data(apikey):
-    file = open('movies-250.txt', 'r', encoding='utf-8')
-    dataset = open(f'movies-{datetime.date.today()}.csv', 'w', newline='', encoding='utf-8')
-    writer = csv.writer(dataset)
-    writer.writerow(['Title', 'Year', 'Rated', 'Genre', 'Plot'])
-    collect = []
-    cannot_collect = []
-    for line in file:
-        title = line.rstrip('\n')
-        r = requests.get(f'http://www.omdbapi.com/?apikey={apikey}&t={title}&plot=full')
-        obj = json.loads(r.text)
-        if 'Error' in obj:
-            cannot_collect.append(title)
-            continue
-        else:
-            collect.append(title)
-        writer.writerow([
-            obj['Title'],
-            obj['Year'],
-            obj['Rated'],
-            str(obj['Genre']).replace(', ', ';'),
-            str(obj['Plot']).replace('"', "'")
-        ])
-    return {
-        'retrieve': collect,
-        'error': cannot_collect
-    }
+class Dataset:
+    def __init__(self, punctuation=False, stop_words=False, stem=False, tokenization=False):
+        self.data = pd.read_csv('data/tmdb_5000_movies.csv')[['genres', 'overview']]
+        self.data.dropna(subset=['overview'], inplace=True)
+        self.__clean()
+        self.__preprocess(punctuation, stop_words, stem, tokenization)
+
+    def __clean(self):
+        data = {
+            'Overview': [],
+            'Western': [],
+            'Drama': [],
+            'Thriller': [],
+            'Mystery': [],
+            'Music': [],
+            'Romance': [],
+            'Action': [],
+            'Adventure': [],
+            'Foreign': [],
+            'Crime': [],
+            'Documentary': [],
+            'Horror': [],
+            'Fantasy': [],
+            'History': [],
+            'Science Fiction': [],
+            'Family': [],
+            'TV Movie': [],
+            'Comedy': [],
+            'Animation': [],
+            'War': []
+        }
+        for index, value in self.data.iterrows():
+            obj = json.loads(value['genres'])
+            if len(obj) == 0:
+                continue
+            data['Overview'].append(value['overview'])
+            genres = []
+            for genre in obj:
+                genres.append(genre['name'])
+            for key in data:
+                if key == 'Overview':
+                    continue
+                elif key in genres:
+                    data[key].append(1)
+                else:
+                    data[key].append(0)
+        self.data = pd.DataFrame(data)
+
+    @staticmethod
+    def __punctuation(text):
+        punc = '''!()-[]{};:'"\,<>’./?@#$%^&*_~'''
+        for word in text:
+            if word in punc:
+                text = text.replace(word, '')
+        return text
+
+    @staticmethod
+    def __stop_words(text):
+        stop_words = set(stopwords.words('english'))
+        new_text = []
+        for word in word_tokenize(text):
+            if word.lower() not in stop_words:
+                new_text.append(word)
+        return ' '.join(new_text)
+
+    @staticmethod
+    def __stem(text):
+        ps = PorterStemmer()
+        new_test = []
+        for word in word_tokenize(text):
+            new_test.append(ps.stem(word))
+        return ' '.join(new_test)
+
+    @staticmethod
+    def __tokenization(text):
+        return word_tokenize(text)
+
+    def __preprocess(self, punctuation, stop_words, stem, tokenization):
+        if punctuation:
+            self.data['Overview'] = self.data['Overview'].apply(lambda row: Dataset.__punctuation(row))
+        if stop_words:
+            self.data['Overview'] = self.data['Overview'].apply(lambda row: Dataset.__stop_words(row))
+        if stem:
+            self.data['Overview'] = self.data['Overview'].apply(lambda row: Dataset.__stem(row))
+        if tokenization:
+            self.data['Overview'] = self.data['Overview'].apply(lambda row: Dataset.__tokenization(row))
+
+    def get_dataset(self):
+        return self.data
+
+    def train_test_split(self, test_size=0.2):
+        x_train, x_test, y_train, y_test = train_test_split(self.data[['Overview']], self.data[
+            ['Western', 'Drama', 'Thriller', 'Mystery', 'Music', 'Romance', 'Action', 'Adventure', 'Foreign', 'Crime',
+             'Documentary', 'Horror', 'Fantasy', 'History', 'Science Fiction', 'Family', 'TV Movie', 'Comedy',
+             'Animation', 'War']], test_size=test_size)
+        return x_train, x_test, y_train, y_test
+
 
 if __name__ == '__main__':
-    obj = collect_data('')
-    print(obj['error'])
+    dataset = Dataset(punctuation=True, stop_words=True, tokenization=True)
+    x_train, x_test, y_train, y_test = dataset.train_test_split()
